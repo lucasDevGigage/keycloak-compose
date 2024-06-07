d9d94d01ca0327a47affae8155481b712c1e1760
Lucas Pires <treinamento@gigage.dev>
2024-06-07 14:52:10 -0300

    connect to front

---
+++ frontend/src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import {onMounted, ref} from "vue";
+import {auth} from "./auth";
+
+const data = ref();
+
+onMounted(async () => {
+  const client = await auth();
+
+  data.value = await client?.loadUserInfo();
+});
+</script>
+
+<template>
+  <div>
+    {{ data }}
+  </div>
+</template>
